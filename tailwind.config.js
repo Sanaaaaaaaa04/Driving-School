644e07888e6441c2e36073f8c836e19e282670a5
SpringBootCrudApp <123sanask@gmail.com>
2024-11-11 12:50:38 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Courgette':['Courgette', 'sans-serif'],
+        'Oswald' :['Oswald', 'sans-serif'],
+        'Montserrat' :['Montserrat'],
+        'Open-sans' :['Open-sans']
+      }
+    },
+  },
+  plugins: [],
+}
+
